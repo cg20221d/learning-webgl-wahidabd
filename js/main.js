8f01fb6997dd4216a1384b024a8cc1d1ffeca669
wahidabd <wahed.blog99@gmail.com>
2022-09-07 15:20:37 +0700

    add color to canvas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
 function main() {
-       
+    var kanvas = document.getElementById("kanvas");
+    var gl = kanvas.getContext("webgl");
+
+    // Vertex shader
+    var vertexShaderCode = 
+    "void main() {" + 
+    "}";
+    var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShaderObject, vertexShaderCode);
+    gl.compileShader(vertexShaderObject);
+
+    // Fragment shader
+    var fragmentShaderCode = `
+    void main() {
+
+    }
+    `;
+    var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
+    gl.compileShader(fragmentShaderObject);
+
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShaderObject);
+    gl.attachShader(shaderProgram, fragmentShaderObject);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+
+    gl.clearColor(1.0, 0.65, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 }
