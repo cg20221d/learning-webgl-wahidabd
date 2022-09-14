0cda6cd9df969d5f9c26229acb9e70e468452651
wahidabd <wahed.blog99@gmail.com>
2022-09-14 15:19:04 +0700

    learn array draw

--- js/main.js
+++ js/main.js
@@ -2,12 +2,24 @@
 function main() {
     var kanvas = document.getElementById("kanvas");
     var gl = kanvas.getContext("webgl");
+    
+    var vertices = [
+        0.5, 0.5, // A -> kanan atas
+        0.0, 0.0, // B -> bawah tengah
+        -0.5, 0.5, // C -> kiri atas
+        0.0, 1.0 // D -> atas tengah
+    ];
+
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
     // Vertex shader
     var vertexShaderCode = `
+    attribute vec2 aPosition;
     void main() {
-        float x = 0.0;
-        float y = 0.0;
+        float x = aPosition.x;
+        float y = aPosition.y;
         gl_PointSize = 10.0;
         gl_Position = vec4(x, y, 0.0, 1.0);
     }
@@ -38,8 +50,22 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
+
+    // Kita mengajari GPU bagaimana caranya mengoleksi
+    // nilai posisi dari ARRAY_BUFFER
+    // untuk setiap verteks yang sedang diproses
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
     gl.clearColor(1.0, 0.65, 0.0, 1.0); // Orange
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINTS, 0, 1);
+    // gl.drawArrays(gl.LINES, 0, 3);
+    // gl.drawArrays(gl.LINE_LOOP, 0, 3);
+    // gl.drawArrays(gl.LINE_STRIP, 0, 3);
+    // gl.drawArrays(gl.TRIANGLES, 0, 4);
+    // gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    // gl.drawArrays(gl.POINTS, 0, 4);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
 }
