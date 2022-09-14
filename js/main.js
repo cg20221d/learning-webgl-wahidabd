8c0720b80eb668320cc28116cbe5a31d1f334c51
wahidabd <wahed.blog99@gmail.com>
2022-09-14 14:51:33 +0700

    add dotted

--- js/main.js
+++ js/main.js
@@ -1,21 +1,33 @@
+/**@type{HTMLCanvasElement} */
 function main() {
     var kanvas = document.getElementById("kanvas");
     var gl = kanvas.getContext("webgl");
 
     // Vertex shader
-    var vertexShaderCode = 
-    "void main() {" + 
-    "}";
+    var vertexShaderCode = `
+    void main() {
+        float x = 0.0;
+        float y = 0.0;
+        gl_PointSize = 10.0;
+        gl_Position = vec4(x, y, 0.0, 1.0);
+    }
+    `;
+
     var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShaderObject, vertexShaderCode);
     gl.compileShader(vertexShaderObject);
 
     // Fragment shader
     var fragmentShaderCode = `
+    precision mediump float;
     void main() {
-
+        float r = 0.0;
+        float g = 0.0;
+        float b = 1.0;
+        gl_FragColor = vec4(r, g, b, 1.0);
     }
     `;
+
     var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
     gl.compileShader(fragmentShaderObject);
@@ -26,6 +38,8 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
-    gl.clearColor(1.0, 0.65, 0.0, 1.0);
+    gl.clearColor(1.0, 0.65, 0.0, 1.0); // Orange
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, 1);
 }
