0274e29c938ec10aad57cfe7a5800b9cd6696a4c
wahidabd <wahed.blog99@gmail.com>
2022-09-28 13:59:59 +0700

    add three js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-/**@type{HTMLCanvasElement} */
 function main() {
     var kanvas = document.getElementById("kanvas");
     var gl = kanvas.getContext("webgl");
-    
+
     var vertices = [
-        0.5, 0.5, // A -> kanan atas
-        0.0, 0.0, // B -> bawah tengah
-        -0.5, 0.5, // C -> kiri atas
-        0.0, 1.0 // D -> atas tengah
+        0.5, 0.0, 0.0, 1.0, 1.0,   // A: kanan atas    (BIRU LANGIT)
+        0.0, -0.5, 1.0, 0.0, 1.0,  // B: bawah tengah  (MAGENTA)
+        -0.5, 0.0, 1.0, 1.0, 0.0,  // C: kiri atas     (KUNING)
+        0.0, 0.5, 1.0, 1.0, 1.0    // D: atas tengah   (PUTIH)
     ];
 
     var buffer = gl.createBuffer();
@@ -15,57 +14,110 @@ function main() {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
     // Vertex shader
-    var vertexShaderCode = `
+    var vertexShaderCode =  `
     attribute vec2 aPosition;
+    attribute vec3 aColor;
+    uniform float uTheta;
+    uniform vec2 uDelta;
+    varying vec3 vColor;
     void main() {
-        float x = aPosition.x;
-        float y = aPosition.y;
-        gl_PointSize = 10.0;
-        gl_Position = vec4(x, y, 0.0, 1.0);
+        float x = -sin(uTheta) * aPosition.x + cos(uTheta) * aPosition.y;
+        float y = cos(uTheta) * aPosition.x + sin(uTheta) * aPosition.y;
+        gl_Position = vec4(x + uDelta.x, y + uDelta.y, 0.0, 1.0);
+        vColor = aColor;
     }
     `;
-
     var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShaderObject, vertexShaderCode);
-    gl.compileShader(vertexShaderObject);
+    gl.compileShader(vertexShaderObject);   // sampai sini sudah jadi .o
 
     // Fragment shader
     var fragmentShaderCode = `
     precision mediump float;
+    varying vec3 vColor;
     void main() {
-        float r = 0.0;
-        float g = 0.0;
-        float b = 1.0;
-        gl_FragColor = vec4(r, g, b, 1.0);
+        gl_FragColor = vec4(vColor, 1.0);
     }
     `;
-
     var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
-    gl.compileShader(fragmentShaderObject);
+    gl.compileShader(fragmentShaderObject);   // sampai sini sudah jadi .o
 
-    var shaderProgram = gl.createProgram();
+    var shaderProgram = gl.createProgram(); // wadah dari executable (.exe)
     gl.attachShader(shaderProgram, vertexShaderObject);
     gl.attachShader(shaderProgram, fragmentShaderObject);
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
+    // Variabel lokal
+    var theta = 0.0;
+    var freeze = false;
+    var horizontalSpeed = 0.0;
+    var verticalSpeed = 0.0;
+    var horizontalDelta = 0.0;
+    var verticalDelta = 0.0;
+
+    // Variabel pointer ke GLSL
+    var uTheta = gl.getUniformLocation(shaderProgram, "uTheta");
+    var uDelta = gl.getUniformLocation(shaderProgram, "uDelta");
 
     // Kita mengajari GPU bagaimana caranya mengoleksi
-    // nilai posisi dari ARRAY_BUFFER
-    // untuk setiap verteks yang sedang diproses
+    //  nilai posisi dari ARRAY_BUFFER
+    //  untuk setiap verteks yang sedang diproses
     var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 
+        5 * Float32Array.BYTES_PER_ELEMENT, 
+        0);
     gl.enableVertexAttribArray(aPosition);
+    var aColor = gl.getAttribLocation(shaderProgram, "aColor");
+    gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 
+        5 * Float32Array.BYTES_PER_ELEMENT, 
+        2 * Float32Array.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(aColor);
 
-    gl.clearColor(1.0, 0.65, 0.0, 1.0); // Orange
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    // Grafika interaktif
+    // Tetikus
+    function onMouseClick(event) {
+        freeze = !freeze;
+    }
+    document.addEventListener("click", onMouseClick);
+    // Papan ketuk
+    function onKeydown(event) {
+        if (event.keyCode == 32) freeze = !freeze;  // spasi
+        // Gerakan horizontal: a ke kiri, d ke kanan
+        if (event.keyCode == 65) {  // a
+            horizontalSpeed = -0.01;
+        } else if (event.keyCode == 68) {   // d
+            horizontalSpeed = 0.01;
+        }
+        // Gerakan vertikal: w ke atas, s ke bawah
+        if (event.keyCode == 87) {  // w
+            verticalSpeed = -0.01;
+        } else if (event.keyCode == 83) {   // s
+            verticalSpeed = 0.01;
+        }
+    }
+    function onKeyup(event) {
+        if (event.keyCode == 32) freeze = !freeze;
+        if (event.keyCode == 65 || event.keyCode == 68) horizontalSpeed = 0.0;
+        if (event.keyCode == 87 || event.keyCode == 83) verticalSpeed = 0.0;
+    }
+    document.addEventListener("keydown", onKeydown);
+    document.addEventListener("keyup", onKeyup);
 
-    // gl.drawArrays(gl.LINES, 0, 3);
-    // gl.drawArrays(gl.LINE_LOOP, 0, 3);
-    // gl.drawArrays(gl.LINE_STRIP, 0, 3);
-    // gl.drawArrays(gl.TRIANGLES, 0, 4);
-    // gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-    // gl.drawArrays(gl.POINTS, 0, 4);
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+    function render() {
+        gl.clearColor(1.0,      0.65,    0.0,    1.0);  // Oranye
+        //            Merah     Hijau   Biru    Transparansi
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        if (!freeze) {
+            theta += 0.1;
+            gl.uniform1f(uTheta, theta);
+        }
+        horizontalDelta += horizontalSpeed;
+        verticalDelta -= verticalSpeed;
+        gl.uniform2f(uDelta, horizontalDelta, verticalDelta);
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
 }
